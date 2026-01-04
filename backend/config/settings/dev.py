560fe0e8343bd65b608f50537c831e50a28eb47a
Jamil Hossain <jamilhossain3251@gmail.com>
2026-01-05 03:30:24 +0600

    Update dev settings

--- backend/config/settings/dev.py
+++ backend/config/settings/dev.py
@@ -4,4 +4,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
+# Use SQLite for local development
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
