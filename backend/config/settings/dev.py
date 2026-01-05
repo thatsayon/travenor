8dbd3df601957c9c44b1bb3454f4f2cc1273f08a
Ashiqul Islam Ayon <ashiqulislamayon28@gmail.com>
2026-01-05 06:27:47 +0600

    auth bug fixed

--- backend/config/settings/dev.py
+++ backend/config/settings/dev.py
@@ -9,11 +9,13 @@ CORS_ALLOWED_ORIGINS = [
     "https://travenor.projectyard.top",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://travenor-v1.thatsayon.com",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "https://travenor.projectyard.top",
     "http://localhost:3000",
+    "https://travenor-v1.thatsayon.com",
 ]
 
 # Use SQLite for local development
