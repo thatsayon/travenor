f44a36aab2b7efc5e9cb5b464c56b3b01ae47d9e
Ashiqul Islam Ayon <ashiqulislamayon28@gmail.com>
2026-01-15 00:13:53 +0600

    access token time updated

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -189,7 +189,7 @@ REST_FRAMEWORK = {
 
 # JWT
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
