02076029a7f17d37d5375708a4929bfd3334b1fa
Ashiqul Islam Ayon <ashiqulislamayon28@gmail.com>
2026-03-12 06:30:00 +0600

    config.cjs fixed

--- backend/ecosystem.config.cjs
+++ backend/ecosystem.config.cjs
@@ -33,8 +33,8 @@ module.exports = {
             args: "-A config worker --loglevel=info --concurrency=2 -Q celery",
             env: {
                 DJANGO_SETTINGS_MODULE: "config.settings.prod",
-                CELERY_BROKER_URL: "redis://localhost:6379/0",
-                CELERY_RESULT_BACKEND: "redis://localhost:6379/0",
+                CELERY_BROKER_URL: "redis://127.0.0.1:6379/0",
+                CELERY_RESULT_BACKEND: "redis://127.0.0.1:6379/0",
             },
             autorestart: true,
             watch: false,
